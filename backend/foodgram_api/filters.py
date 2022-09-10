d52a9cc1f15523b17da9cf3084c7e06a8ba8b977
гомеля максим <maksimgomelya@mail.ru>
2022-09-10 15:31:54 +0300

    Filter authors

--- backend/foodgram_api/filters.py
+++ backend/foodgram_api/filters.py
@@ -1,11 +1,15 @@
 from django_filters import rest_framework as filters
 
-from .models import Recipe
+from .models import Recipe, User
 
 
 class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
 
     class Meta:
         model = Recipe
-        fields = ['tags']
+        fields = [
+            'tags',
+            'author',
+        ]
