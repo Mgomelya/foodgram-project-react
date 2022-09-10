8a90ee69e2275fef12dcab6a4e23e663ed1fcfad
гомеля максим <maksimgomelya@mail.ru>
2022-09-10 18:22:10 +0300

    ci

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY",
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", 1))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['foddcorzhh.ddns.net']
 
 # Application definition
 
@@ -73,17 +73,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-    #     'NAME': os.getenv('DB_NAME', 'postgres'),
-    #     'USER': os.getenv('POSTGRES_USER', 'postgres'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-    #     'HOST': os.getenv('DB_HOST', 'db'),
-    #     'PORT': os.getenv('DB_PORT', 5432)
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
